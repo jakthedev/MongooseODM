3ec75a3598f082d56cabfb3dbf409e62cb258776
JD Alexander Kemp <jdakemp@gmail.com>
2020-10-18 15:57:09 -0400

    created subArray of comments within the dishes schema

--- index.js
+++ index.js
@@ -15,10 +15,25 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {description: 'Updated test'}
+            },{
+        }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\ m getting a sinking feeling!',
+                author: 'Leonard di Carpaccio'
+            });
+
+            return dish.save();
+
+        })
+        .then((dish) => {
+            console.log(dish);
 
             return Dishes.remove({});
         })
